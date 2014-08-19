42b98dae36a9b8114b94149ecfb494ec5c0e8545
richardmillson <8217613+richardmillson@users.noreply.github.com>
2014-08-19 15:33:09 -0400

    added Mathematica to LaTeX function

--- matrix-detex.py
+++ matrix-detex.py
@@ -1,7 +1,12 @@
 
 from Tkinter import Tk
 
-def texToMat():
+def copy(s):
+    Tk().clipboard_clear()
+    Tk().clipboard_append(s)
+    Tk().destroy()
+
+def toMat():
     s = Tk().clipboard_get()
     print s
 
@@ -10,11 +15,22 @@ def texToMat():
     if "\\end{" in s:
         s = s[:s.rfind('\\', 0, len(s))]
     s = s.replace(" ", "")
+    s = s.replace("\n", "")
     s = s.replace("&", ",")
     s = s.replace("\\\\", "},{")
     s = "{{" + s + "}}"
 
     print s
-    Tk().clipboard_clear()
-    Tk().clipboard_append(s)
-    Tk().destroy()
+    copy(s)
+
+def toTex():
+    s = Tk().clipboard_get()
+    print s
+    
+    s = s.replace("{{", "\\begin{bmatrix} \n")
+    s = s.replace("}}", "\n\\end{bmatrix}")
+    s = s.replace("},{", "\\\\ \n")
+    s = s.replace(",", "&")
+    
+    print s
+    copy(s)
