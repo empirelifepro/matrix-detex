6ec4a71ae38071088d78609e329ad93db6097d23
richardmillson <8217613+richardmillson@users.noreply.github.com>
2014-09-26 21:52:26 -0400

    Add .travis.yml and support tests

--- matrix_detex_test.py
+++ matrix_detex_test.py
@@ -1,10 +1,9 @@
 from matrix_detex import *
 
-
 def test():
-    root = Tk()
-    s = "\\begin{bmatrix} 1 & 0 \\\\ 0 & 1 \\end{bmatrix}"
-    copy(s , root)
-    toMat()
-    
-test()    
+	root = Tk()
+	s = "\\begin{bmatrix} a \cdot b & 0 \\\\ 0 & 1 \\end{bmatrix} \\begin{bmatrix} 0 & 1 \\\\ 1 & 0 \\end{bmatrix}"
+	#s = "\left( 13 \cdot 6 \right) \cdot \left( 12 \cdot 6 \right) \cdot \left( 11 \cdot 4 \right)"
+	copy(s , root)
+	toMat()
+	return root.clipboard_get() == "{{a*b,0}{0,1}}{{0,1},{1,0}}"
