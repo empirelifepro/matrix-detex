82de4b29a613109becf6d4e3cec8ea7f1f96f040
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-10-08 11:13:09 -0400

    Conform to Mathematica formatting of matrices

--- matrix_detex.py
+++ matrix_detex.py
@@ -6,34 +6,42 @@ def copy(s, root):
     root.clipboard_clear()
     root.clipboard_append(s)
     
-def toMat(s):
+def to_mat(s):
     s = re.sub(r'\\begin{.+?}', '{{', s)
     s = re.sub(r'\\end{.+?}', '}}', s)
     s = s.replace(' ', '')
     s = s.replace('\n', '')
-    s = s.replace('&', ',')
-    s = s.replace('\\\\', '},{')
+    s = s.replace('&', ', ')
+    s = s.replace('\\\\', '}, {')
     s = s.replace('\cdot', '*')
     s = s.replace('\left', '')
     s = s.replace('\right', '')
     return s
 
-def toTex(s):
-    s = s.replace("{{", "\\begin{bmatrix} \n")
-    s = s.replace("}}", "\n\\end{bmatrix}")
-    s = s.replace("},{", "\\\\\n")
-    s = s.replace(",", "&")
-    s = s.replace("*", " \\cdot ")
+def to_tex(s):
+    s = s.replace('{{', '\\begin{bmatrix} \n')
+    s = s.replace('}}', '\n\\end{bmatrix}')
+    s = s.replace('}, {', '\\\\\n')
+    s = s.replace(',', ' &')
+    s = s.replace('*', ' \\cdot ')
     return s
 
-def main():
+def from_clipboard():
     root = Tk()
     s = root.clipboard_get()
     print s
     if s[0] == '{':
-        out = toTex(s)
+        out = to_tex(s)
     else:
-        out = toMat(s)
+        out = to_mat(s)
     print out
     copy(out, root)
-    root.destroy()  
+    root.destroy() 
+
+def from_arg(s):    
+    print s
+    if s[0] == '{':
+        out = to_tex(s)
+    else:
+        out = to_mat(s)
+    print out
--- matrix_detex_test.py
+++ matrix_detex_test.py
@@ -1,9 +1,9 @@
 from matrix_detex import *
 
-def test_toMat():
-	s = "\\begin{bmatrix} a \cdot b & 0 \\\\ 0 & 1 \\end{bmatrix} \\begin{bmatrix} 0 & 1 \\\\ 1 & 0 \\end{bmatrix}"
-	assert toMat(s)  == "{{a*b,0},{0,1}}{{0,1},{1,0}}"
+def test_to_mat():
+	s = '\\begin{bmatrix} a \cdot b & 0 \\\\ 0 & 1 \\end{bmatrix} \\begin{bmatrix} 0 & 1 \\\\ 1 & 0 \\end{bmatrix}'
+	assert to_mat(s)  == '{{a*b, 0}, {0, 1}}{{0, 1}, {1, 0}}'
 
-def test_toTex():
-	s = "{{a*b,0},{0,1}}{{0,1},{1,0}}"
-	assert toTex(s)  == "\\begin{bmatrix} \na \cdot b&0\\\\\n0&1\n\\end{bmatrix}\\begin{bmatrix} \n0&1\\\\\n1&0\n\\end{bmatrix}"
+def test_to_tex():
+	s = '{{a*b, 0}, {0, 1}}{{0, 1}, {1, 0}}'
+	assert to_tex(s)  == '\\begin{bmatrix} \na \\cdot b & 0\\\\\n0 & 1\n\\end{bmatrix}\\begin{bmatrix} \n0 & 1\\\\\n1 & 0\n\\end{bmatrix}'
